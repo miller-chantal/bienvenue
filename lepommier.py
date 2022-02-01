7c07a1c48c0856183b7a41bb2cafc6bfdc99e213
miller-chantal <miller.chantal.shari@gmail.com>
2022-02-01 12:32:32 -0500

    a brief introduction

---
+++ lepommier.py
@@ -0,0 +1,14 @@
+#welcome to my github page
+
+import datetime
+today = datetime.datetime.now()
+
+print("Hello. Welcome to my github page")
+
+print("""I am learning python
+and so far, my favorite module is datetime
+
+
+Thank you
+-C Miller   """ + today.strftime('%A %B %d %Y'))
+
